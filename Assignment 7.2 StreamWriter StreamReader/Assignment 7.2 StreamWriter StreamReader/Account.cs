20459fc904931b7c3447d512a8bc4fc7d0965db7
SuyashSPulate <suyashpulate@gmail.com>
2022-11-15 13:55:40 +0530

    7th assignment uploaded

---
+++ Assignment 7.2 StreamWriter StreamReader/Assignment 7.2 StreamWriter StreamReader/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_7._2_StreamWriter_StreamReader
+{
+    internal class Account
+    {
+        public int account_number;
+        public string customer_name;
+        public double balance;
+
+
+        public Account(int accno, string name, double bal)
+        {
+            account_number = accno;
+            customer_name = name;
+            balance = bal;
+        }
+    }
+}
